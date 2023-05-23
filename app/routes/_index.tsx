0c4884b6cb2c5967772ff38761f839b2de3413ea
MA514R7UBY <ing.marcoslopezsanchez@gmail.com>
2023-05-23 13:19:52 -0600

    Practica
    Nada importante.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,5 +7,20 @@ export const links: LinksFunction = () => [
 ];
 
 export default function IndexRoute() {
-  return <div>Hello Marcos Index Route</div>;
+  return (
+    <section className="hero" id="hero">
+    <img src="assets/images/Junior.svg" alt="Marcos-Lopez" loading="lazy" className="hero-img" />
+    <div className="hero-bio animate__animated animate__shakeX">
+      <h2 className="hero-bio-titulo">Sobre Mí</h2>
+      <p className="hero-bio-texto">
+        Soy una persona capaz de trabajar en equipo aportando ideas, debido a mi experiencia
+        como Operador de Recepción Digital era necesario la comunicación entre departamento
+        para ser eficaz también ser multitarea manteniéndome en comunicación con mi equipo,
+        debido a ser un aprendiz proactivo me he dado a la tarea de esforzarme en aprender
+        para mejorar constantemente.
+      </p>
+    </div>
+  </section>
+  )
+  ;
 }
