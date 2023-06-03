feb5da033572fcdded6372370ba63f41296c9d85
Marcos Imac <marcoslopezsanchez@hotmail.com>
2023-06-03 14:54:22 -0600

    styles css root
    start css

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,7 @@ export default function App() {
           name="viewport"
           content="width=device-width,initial-scale=1"
         />
-        <title>Portafolio</title>
+        <title>Portafolio1</title>
         <Links/>
       </head>
       <body>
--- app/routes/portafolio.tsx
+++ app/routes/portafolio.tsx
@@ -1,19 +1,26 @@
+import type { LinksFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
-import {Link} from "@remix-run/react";
+import { Link } from "@remix-run/react";
+
+import stylesUrl from "~/styles/portfolio.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
 
 export default function JokesRoute() {
   return (
-    <div>
-      <h1 className="home-link">
-            <Link
-              to="/"
-              title="Remix Jokes"
-              aria-label="Remix Jokes"
-            >
-              <span className="logo">🤪f</span>
+    <div className="portafolio-layout">
+      <header className="portafolio-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
+              <span className="logo">🤪</span>
               <span className="logo-medium">PORTAFOLIO</span>
             </Link>
           </h1>
+        </div>
+      </header>
       <main>
         <Outlet />
       </main>
