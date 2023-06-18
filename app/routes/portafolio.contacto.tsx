7482c04fb3c7d7206025f7cc370a2dfa5fc26c12
MARCOS <marcoslopezsanchez@hotmail.com>
2023-06-18 00:32:03 -0600

    css contact

--- app/routes/portafolio.contacto.tsx
+++ app/routes/portafolio.contacto.tsx
@@ -1,3 +1,10 @@
+import type { LinksFunction } from "@remix-run/node";
+import stylesUrl from "~/styles/contactme.css"; 
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
+
 export default function ContactoPortafolioRoute() {
     return (
       <div className="grid">
