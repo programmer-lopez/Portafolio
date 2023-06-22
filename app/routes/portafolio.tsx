bc9ad1e8b36e51701e00fb76332540f748c2ee31
MA514R7UBY <ing.marcoslopezsanchez@gmail.com>
2023-06-21 18:05:43 -0600

    add center perfile

--- app/routes/portafolio.tsx
+++ app/routes/portafolio.tsx
@@ -13,17 +13,10 @@ export default function PortafoliosRoute() {
     <div className="portafolio-layout">
       <header className="portafolio-header">
         <div className="container">
-          <img 
-          src={portafolio} 
-          alt="Portafolio" 
-          className="portafolio-logo"
-           />
+          <img src={portafolio} alt="Portafolio" className="portafolio-logo" />
           <h2 className="home-link">
-            <Link to="/"
-             title="Portafolio" aria-label="Remix Portafolio">
-              <span 
-              className="logo-medium">Developer with TypesScript
-              </span>
+            <Link to="/" title="Portafolio" aria-label="Remix Portafolio">
+              <span className="logo-medium">Developer with TypesScript</span>
             </Link>
           </h2>
         </div>
@@ -31,20 +24,23 @@ export default function PortafoliosRoute() {
       <main className="portafolio-main">
         <div className="portafolio__container-perfile">
           <div className="portafolio-list">
-            <ul>
-              <li>
-                <Link to=".">Portafolio</Link>
-              </li>
-              <li>
-                <Link to="about">About</Link>
-              </li>
-              <li>
-                <Link to="experiencia">Experiencia</Link>
-              </li>
-              <li>
-                <Link to="contacto">Contacto </Link>
-              </li>
-            </ul>
+            <div className="portafolio-perfile">
+             <h5>Marcos Lopez</h5> 
+              <ul>
+                <li>
+                  <Link to=".">Portafolio</Link>
+                </li>
+                <li>
+                  <Link to="about">About</Link>
+                </li>
+                <li>
+                  <Link to="experiencia">Experiencia</Link>
+                </li>
+                <li>
+                  <Link to="contacto">Contacto </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="portafolio-outlet">
