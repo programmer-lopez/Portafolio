9f5976cac4d5c048537d6a56cfe502a84bc6e4f1
MA514R7UBY <ing.marcoslopezsanchez@gmail.com>
2023-07-03 12:00:13 -0600

    portafolio perfile

--- app/routes/portafolio._index.tsx
+++ app/routes/portafolio._index.tsx
@@ -1,5 +1,6 @@
 import { LinksFunction } from "@remix-run/node";
 import Me from "../../public/img/about.jpg";
+import perfile from "../../public/img/perfile.png";
 import stylesUrl from "./styles/index.css";
 
 export const links: LinksFunction = () => [
@@ -9,15 +10,20 @@ export const links: LinksFunction = () => [
 export default function PortafolioIndexRoute() {
   return (
     <div className="portafolio_acerca">
-      <h3>Hola, Me llamo Marcos Lopez Sanchez</h3>
-      <div className="portafolio__acerca_info">
-        <h4>Desarrollador de Software</h4>
-        <p>
-          Aspiro como Desarrollador de software, experiencia en React.tsx como
-          Junior Developer.
-        </p>
-      </div>
-      <img src={Me} alt="Acerca-de-Mi" />
+      <section id="portafolio_index">
+        <div className="section-developer">
+          <img className="perfile-image" src={perfile} alt="Marcos_Lopez" />
+          <h3>Hola, Me llamo Marcos Lopez Sanchez</h3>
+          <div className="portafolio__acerca_info">
+            <h4>Desarrollador de Software</h4>
+            <p>
+              Aspiro como Desarrollador de software, experiencia en React.tsx
+              como Junior Developer.
+            </p>
+            <img className="acerca-de-mi" src={Me} alt="Acerca-de-Mi" />
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
