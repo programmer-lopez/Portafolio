2680c4e9e96344abe8841bdd286dcc15dd74c364
Marcos Imac <marcoslopezsanchez@hotmail.com>
2023-08-08 12:50:59 -0600

    Experience: Add info

--- app/routes/portafolio.experiencia.tsx
+++ app/routes/portafolio.experiencia.tsx
@@ -10,18 +10,73 @@ export const links: LinksFunction = () => [
 export default function ExperienciaPortafolioRoute() {
   return (
     <div>
+      <main className="content">
       <h1>Experiencia</h1>
-      <main>
-      
-      <hr />
+      <h2 className="Subtitle">Clonaciones</h2>
+      <p>
+        La experiencia obtenida es por el aprendizaje General de las tecnología.
+      </p>
+        <hr />
         <section>
           <div className="container">
             <div className="card">
-              <h2>
-                <span>Desarrollador web</span>
+              <h2 className="title">
+                <span>Cascada Santiago Yosondúa</span>
               </h2>
               <div className="bar">
                 <div className="emptybar"></div>
+                <p>
+                  -Pagina web Realizada con las siguentes herramientas: Html,
+                  Css, JavaScript.
+                </p>
+                <p>
+                  -Pagina web Realizada con las siguentes herramientas: Html,
+                  Css, JavaScript.
+                </p>
+                <div className="filledbar"></div>
+                <a
+                  className=""
+                  href="https://cascada-yosondua-no-oficial.netlify.app/"
+                >
+                  <RiPagesFill />
+                </a>
+              </div>
+              <div className="circle">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                  <circle className="stroke" cx={60} cy={98} r={50} />
+                </svg>
+              </div>
+            </div>
+            <div className="card">
+              <h2 className="title">
+                <span>Publivalle Dania</span>
+              </h2>
+              <div className="bar">
+                <div className="emptybar"></div>
+                <p>
+                  -Pagina web Realizada con las siguentes herramientas: Html,
+                  Css, JavaScript.
+                </p>
+                <div className="filledbar"></div>
+                <a className="" href="https://publivalle-dania.netlify.app/">
+                  <RiPagesFill />
+                </a>
+              </div>
+              <div className="circle">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                  <circle className="stroke" cx={60} cy={98} r={50} />
+                </svg>
+              </div>
+            </div>
+            <div className="card">
+              <h2 className="title">
+                <span>Mecado Libre</span>
+              </h2>
+              <div className="bar">
+                <div className="emptybar"></div>
+                <p>
+                  -Fueron realizadas con tecnologias: React( Remix), React js.
+                </p>
                 <div className="filledbar"></div>
               </div>
               <div className="circle">
@@ -30,55 +85,26 @@ export default function ExperienciaPortafolioRoute() {
                 </svg>
               </div>
             </div>
-            
+            <div className="card">
+              <h2 className="title">
+              <span>Portafolio en Remix</span>
+              </h2>
+              <div className="bar">
+                <div className="emptybar"></div>
+                <a className="" href="https://mercado-libre-clone.netlify.app/">
+                  <RiPagesFill />
+                </a>
+                <div className="filledbar"></div>
+              </div>
+              <div className="circle">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                  <circle className="stroke" cx={60} cy={98} r={50} />
+                </svg>
+              </div>
+            </div>
           </div>
         </section>
       </main>
     </div>
   );
 }
-
-/*<h2>
-              <span>Clonaciones</span>
-            </h2>
-            <p>
-              La experiencia obtenida es por el aprendizaje General de las
-              tecnología.
-            </p>
-            <div className="carouse">
-              <div className="elements">
-                <h4>Cascada Santiago Yosondúa</h4>
-                <p>
-                  -Pagina web Realizada con las siguentes herramientas: Html,
-                  Css, JavaScript.
-                </p>
-                <a
-                  className=""
-                  href="https://cascada-yosondua-no-oficial.netlify.app/"
-                >
-                  <RiPagesFill />
-                </a>
-              </div>
-              <div className="elements">
-                <h4>Publivalle Dania </h4>
-                <p>
-                  -Pagina web Realizada con las siguentes herramientas: Html,
-                  Css, JavaScript.
-                </p>
-                <a className="" href="https://publivalle-dania.netlify.app/">
-                  <RiPagesFill />
-                </a>
-              </div>
-              <div className="elements">
-                <h4>Mercado Libre </h4>
-                <p>
-                  -Fueron realizadas con tecnologias: React( Remix), React js.
-                </p>
-                <a className="" href="https://mercado-libre-clone.netlify.app/">
-                  <RiPagesFill />
-                </a>
-              </div>
-              <div className="elements">
-                <h4>Portafolio</h4>
-              </div>
-            </div> */
