b792b449a2130243f623dd04062600fe6eea99a9
Marcos Imac <marcoslopezsanchez@hotmail.com>
2023-08-09 17:43:39 -0600

    pruebas

--- app/routes/portafolio._index.tsx
+++ app/routes/portafolio._index.tsx
@@ -6,8 +6,7 @@ import perfile from "../../public/img/perfile.png";
 import stylesUrl from "./styles/portaIndex.css";
 import { FaReact, FaJava, FaFileCode, FaMdb } from "react-icons/fa6";
 
-import React from "react";
-import { Avatar } from "@nextui-org/react";
+import { Avatar, AvatarGroup, AvatarIcon } from "@nextui-org/react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
@@ -81,39 +80,29 @@ export default function PortafolioIndexRoute() {
               </div>
             </div>
           </div>
+          <div className="flex gap-4 items-center">
+            <Avatar
+              src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+              className="w-6 h-6 text-tiny"
+            />
+            <Avatar
+              src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
+              size="sm"
+            />
+            <Avatar
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+              size="md"
+            />
+            <Avatar
+              src="https://i.pravatar.cc/150?u=a04258114e29026302d"
+              size="lg"
+            />
+            <Avatar
+              src="https://i.pravatar.cc/150?u=a04258114e29026708c"
+              className="w-20 h-20 text-large"
+            />
+          </div>
         </section>
-        <div className="flex gap-4 items-center">
-          <Avatar
-            isBordered
-            color="default"
-            src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-          />
-          <Avatar
-            isBordered
-            color="primary"
-            src="https://i.pravatar.cc/150?u=a04258a2462d826712d"
-          />
-          <Avatar
-            isBordered
-            color="secondary"
-            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-          />
-          <Avatar
-            isBordered
-            color="success"
-            src="https://i.pravatar.cc/150?u=a04258114e29026302d"
-          />
-          <Avatar
-            isBordered
-            color="warning"
-            src="https://i.pravatar.cc/150?u=a04258114e29026702d"
-          />
-          <Avatar
-            isBordered
-            color="danger"
-            src="https://i.pravatar.cc/150?u=a04258114e29026708c"
-          />
-        </div>
       </div>
     </main>
   );
