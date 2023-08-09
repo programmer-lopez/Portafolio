ce9c59d19bd32eb7ce7d0e45c6430923e8362e4d
Marcos Imac <marcoslopezsanchez@hotmail.com>
2023-08-08 22:20:23 -0600

    guardado de emergencia

--- app/routes/portafolio.contacto.tsx
+++ app/routes/portafolio.contacto.tsx
@@ -3,6 +3,7 @@ import stylesUrl from "./styles/contactme.css";
 import { Link } from "@remix-run/react";
 
 import video from "../../public/videos/Portafolio.mp4";
+import Junior from "../../public//img/Junior.svg"
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesUrl },
@@ -10,21 +11,41 @@ export const links: LinksFunction = () => [
 
 export default function ContactoPortafolioRoute() {
   return (
-    <div className="contactme">
-      <div className="video">
+    <main>
+      <div className="contactame">
+        <div className="container">
+          <h1>Desarrolador Junior</h1>
+          <h2>Marcos Lopez Sanchez</h2>
+          <h4>PORTAFOLIO CONTACTO</h4>
+          {/*car 1*/}
+          <div className="card">
+           <img src={Junior} alt="" />
+          </div>
+          {/*car 2*/}
+          <div className="card">
+           <img src={Junior} alt="" />
+          </div>
+          {/*car 3*/}
+          <div className="card">
+           <img src={Junior} alt="" />
+          </div>
+        </div>
+        <Link
+          className="button"
+          to="https://drive.google.com/file/d/1I6Gz0FbIle17-6rES018pqQUAK4lWM3f/view?usp=drive_link"
+          download="CV"
+          reloadDocument
+        >
+          <button>
+            <span></span>CV.pdf
+          </button>
+        </Link>
+      </div>
+      {/* <div className="video">
         <video autoPlay muted loop id="myVideo">
           <source src={video} type="video/mp4" />
         </video>
-      </div>
-      <Link className="button"
-        to="https://drive.google.com/file/d/1I6Gz0FbIle17-6rES018pqQUAK4lWM3f/view?usp=drive_link"
-        download="CV"
-        reloadDocument
-      >
-        <button>
-          <span></span>CV.pdf
-        </button>
-      </Link>
-    </div>
+      </div> */}
+    </main>
   );
 }
